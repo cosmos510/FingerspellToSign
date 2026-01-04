173efc2878bec7929411e70a38a6e7842b9d8f5c
Martin Maxime <149656366+cosmos510@users.noreply.github.com>
2026-01-04 21:49:40 +0100

    feat: Add about and predict CSS files for improved styling and layout; optimize CSS loading in templates

---
+++ asl_monolith/static/loadcss.js
@@ -0,0 +1,69 @@
+// CSS Loading optimization
+(function() {
+    'use strict';
+    
+    // Function to load CSS asynchronously
+    function loadCSS(href, before, media) {
+        var doc = window.document;
+        var ss = doc.createElement('link');
+        var ref;
+        if (before) {
+            ref = before;
+        } else {
+            var refs = (doc.body || doc.getElementsByTagName('head')[0]).childNodes;
+            ref = refs[refs.length - 1];
+        }
+        
+        var sheets = doc.styleSheets;
+        ss.rel = 'stylesheet';
+        ss.href = href;
+        ss.media = 'only x';
+        
+        function ready(cb) {
+            if (doc.body) {
+                return cb();
+            }
+            setTimeout(function() {
+                ready(cb);
+            });
+        }
+        
+        ready(function() {
+            ref.parentNode.insertBefore(ss, (before ? ref : ref.nextSibling));
+        });
+        
+        var onloadcssdefined = function(cb) {
+            var resolvedHref = ss.href;
+            var i = sheets.length;
+            while (i--) {
+                if (sheets[i].href === resolvedHref) {
+                    return cb();
+                }
+            }
+            setTimeout(function() {
+                onloadcssdefined(cb);
+            });
+        };
+        
+        function loadCB() {
+            if (ss.addEventListener) {
+                ss.removeEventListener('load', loadCB);
+            }
+            ss.media = media || 'all';
+        }
+        
+        if (ss.addEventListener) {
+            ss.addEventListener('load', loadCB);
+        }
+        ss.onloadcssdefined = onloadcssdefined;
+        onloadcssdefined(loadCB);
+        return ss;
+    }
+    
+    // Expose loadCSS globally
+    if (typeof exports !== 'undefined') {
+        exports.loadCSS = loadCSS;
+    } else {
+        window.loadCSS = loadCSS;
+    }
+}());
